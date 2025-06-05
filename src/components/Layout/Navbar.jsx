0dc0a511467da8bfc5d24205b1491f3343347d68
keshavSharma1017 <keshavbusharma2001@gmail.com>
2025-06-06 00:42:51 +0530

    Updated Navbar.jsx

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useAuth } from '../../../client/src/context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 function Navbar() {
   const { user, logout } = useAuth();
